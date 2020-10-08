c276dfca3a166a6be1ce4a01d9a43b7b6ab9a963
Yiming Pan <3207113562@qq.com>
2020-10-08 14:24:41 +0800

    feat: add 530 solution1 for C++ && JavaScript

---
+++ solution/530/sol1.js
@@ -0,0 +1,18 @@
+var getMinimumDifference = function (root) {
+  let ans = Number.MAX_SAFE_INTEGER, pre = -1;
+  const dfs = (root) => {
+    if (root === null) {
+      return;
+    }
+    dfs(root.left);
+    if (pre == -1) {
+      pre = root.val;
+    } else {
+      ans = Math.min(ans, root.val - pre);
+      pre = root.val;
+    }
+    dfs(root.right);
+  }
+  dfs(root);
+  return ans;
+};
