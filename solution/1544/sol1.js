31bca61f501d60a56c9fe5fd9a5adfe8e4e688e6
PerfectPan <perfectpan325@gmail.com>
2022-04-20 23:40:25 +0800

    feature: add 1544 solution1 for JavaScript

---
+++ solution/1544/sol1.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function(s) {
+    const len = s.length;
+    const ret = [];
+    let i = 0;
+    while (i < len) {
+        if (ret.length > 0 
+        && ret[ret.length - 1].toLowerCase() === s.charAt(i).toLowerCase()
+        && ret[ret.length - 1] !== s.charAt(i)
+        ) {
+            ret.pop();
+        } else {
+            ret.push(s.charAt(i));
+        }
+        i += 1;
+    }
+    return ret.join('');
+};
+
