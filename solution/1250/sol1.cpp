66e24dd329db6bf42a05a53052583886342df197
Yiming Pan <perfectpan325@gmail.com>
2023-02-15 23:46:45 +0800

    feature: add 1250 solution1 for C++

---
+++ solution/1250/sol1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            ans = gcd(ans, nums[i]);
+        }
+        return ans == 1;
+    }
+};
