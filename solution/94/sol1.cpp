b66a35e7116381755a88c69b59860dae3ded558c
Yiming Pan <perfectpan325@gmail.com>
2024-02-10 12:37:33 +0800

    feature: add 94 solution1 for C++

---
+++ solution/94/sol1.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int>& res) {
+        if (root == nullptr) {
+            return;
+        }
+        dfs(root->left, res);
+        res.push_back(root->val);
+        dfs(root->right, res);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        dfs(root, res);
+        return res;
+    }
+};
