080547047e8c1e3e372bc3bd6e18d37702b6b75b
Yiming Pan <perfectpan325@gmail.com>
2024-03-18 12:01:30 +0800

    feature: add 303 solution1 for C++

---
+++ solution/303/sol1.cpp
@@ -0,0 +1,21 @@
+class NumArray {
+private:
+    vector<int> prefix;
+public:
+    NumArray(vector<int>& nums) {
+        prefix.resize(nums.size() + 1);
+        for (int i = 0; i < nums.size(); ++i) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
