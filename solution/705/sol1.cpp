da9a18d52f198fc66fe3ee8adf3aacb2368d9f0a
Yiming Pan <perfectpan325@gmail.com>
2024-04-14 15:01:09 +0800

    feature: add 705 solution1 for C++

---
+++ solution/705/sol1.cpp
@@ -0,0 +1,50 @@
+class MyHashSet {
+private:
+    vector<list<int>> data;
+    static const int base = 769;
+    static int hash(int key) {
+        return key % base;
+    }
+public:
+    MyHashSet(): data(base) {
+
+    }
+    
+    void add(int key) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                return;
+            }
+        }
+        data[h].push_back(key);
+    }
+    
+    void remove(int key) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                data[h].erase(it);
+                return;
+            }
+        }
+    }
+    
+    bool contains(int key) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
