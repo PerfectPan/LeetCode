87eed81bdcc68aa2c27747485b6a858b8609d82b
Yiming Pan <perfectpan325@gmail.com>
2024-05-12 22:48:48 +0800

    feature: add 1553 solution1 for C++

---
+++ solution/1553/sol1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    map<int, int> mp;
+    int minDays(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        if (mp.count(n)) {
+            return mp[n];
+        }
+        return mp[n] = min(n % 2 + 1 + minDays(n / 2), n % 3 + 1 + minDays(n / 3));
+    }
+};
