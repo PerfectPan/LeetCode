26ba53f1e5a821686749882024c94134e0b76624
Yiming Pan <perfectpan325@gmail.com>
2024-05-13 23:26:46 +0800

    feature: update 994 solution1 for C++

--- solution/994/sol1.cpp
+++ solution/994/sol1.cpp
@@ -1,39 +1,42 @@
 class Solution {
+private:
+    constexpr static int dx[4]={0, 1, 0, -1};
+    constexpr static int dy[4]={1, 0, -1, 0};
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>>Q;
-        int t[20][20];
-        int dir_x[]={0,1,0,-1};
-        int dir_y[]={1,0,-1,0};
-        memset(t,-1,sizeof(t));
-        for (int i=0;i<(int)grid.size();++i){
-            for (int j=0;j<(int)grid[i].size();++j){
-                if (grid[i][j]==2){
-                    Q.push(make_pair(i,j));
-                    t[i][j]=0;
+        queue<pair<int,int>> Q;
+        vector<vector<int>> dis(10, vector<int>(10, -1));
+        int cnt = 0;
+        int n = grid.size(), m = grid[0].size(), ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 2) {
+                    Q.push({i, j});
+                    dis[i][j] = 0;
+                } else if (grid[i][j] == 1) {
+                    cnt += 1;
                 }
             }
         }
-        while (!Q.empty()){
-            pair<int,int> x=Q.front();Q.pop();
-            for (int i=0;i<4;++i){
-                int tx=x.first+dir_x[i];
-                int ty=x.second+dir_y[i];
-                if (tx<0||tx>=(int)grid.size()||ty<0||ty>=(int)grid[0].size()||t[tx][ty]!=-1||grid[tx][ty]==0) continue;
-                t[tx][ty]=t[x.first][x.second]+1;
-                Q.push(make_pair(tx,ty));
-            }
-        }
-        int ans=0;
-        for (int i=0;i<(int)grid.size();++i){
-            for (int j=0;j<(int)grid[i].size();++j){
-                if (grid[i][j]!=0){
-                    if (t[i][j]==-1) return -1;
-                    ans=max(ans,t[i][j]);
+        while (!Q.empty()) {
+            auto [x, y] = Q.front(); Q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int tx = x + dx[i];
+                int ty = y + dy[i];
+                if (tx < 0|| tx >= n || ty < 0|| ty >= m || ~dis[tx][ty] || !grid[tx][ty]) {
+                    continue;
+                }
+                dis[tx][ty] = dis[x][y] + 1;
+                Q.push({ tx, ty });
+                if (grid[tx][ty] == 1) {
+                    cnt -= 1;
+                    ans = dis[tx][ty];
+                    if (!cnt) {
+                        break;
+                    }
                 }
             }
         }
-        return ans;
+        return cnt ? -1 : ans;
     }
 };
-
