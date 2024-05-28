22a4b9730e23553054ba04ca4526ca8420cf74ac
Yiming Pan <perfectpan325@gmail.com>
2024-05-28 20:00:14 +0800

    feature: add 2951 solution1 for C++

---
+++ solution/2951/sol1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int> ans;
+        int n = mountain.size();
+        for (int i = 1; i + 1 < n; ++i) {
+            if (mountain[i] > mountain[i - 1] and mountain[i] > mountain[i + 1]) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
