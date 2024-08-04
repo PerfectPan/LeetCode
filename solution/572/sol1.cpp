735e099ae0247739c7ba4ac6fb4b6490489e3957
Yiming Pan <perfectpan325@gmail.com>
2024-08-04 23:20:08 +0800

    feature: add 572 solution1 for C++

---
+++ solution/572/sol1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool check(TreeNode *o, TreeNode *t) {
+        if (!o && !t) return true;
+        if ((o && !t) || (!o && t) || (o->val != t->val)) return false;
+        return check(o->left, t->left) && check(o->right, t->right);
+    }
+
+    bool dfs(TreeNode *o, TreeNode *t) {
+        if (!o) return false;
+        return check(o, t) || dfs(o->left, t) || dfs(o->right, t);
+    }
+
+    bool isSubtree(TreeNode *s, TreeNode *t) {
+        return dfs(s, t);
+    }
+};
