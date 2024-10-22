148457411551f62fabe89a6d050918082a2a7bc4
Yiming Pan <perfectpan325@gmail.com>
2024-10-22 23:41:21 +0800

    feature: add 3184 solution1 for C++

---
+++ solution/3184/sol1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        int ans = 0;
+        for (int i = 0; i < hours.size(); ++i) {
+            for (int j = i + 1; j < hours.size(); ++j) {
+                ans += (hours[i] + hours[j]) % 24 == 0;
+            }
+        }
+        return ans;
+    }
+};
