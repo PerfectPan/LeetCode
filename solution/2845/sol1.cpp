e525d37f82e1c19d5193dbaa0440381ebfd4fd78
Yiming Pan <perfectpan325@gmail.com>
2025-04-25 14:19:59 +0800

    feature: add 2845 solution1 for C++

--- solution/2845/sol1.cpp
+++ solution/2845/sol1.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
         long long ans = 0;
-        unordered_map<int, int> mp;
-        int pre = 0;
+        map<int, int> mp;
         mp[0] = 1;
-        for (int i = 0; i < nums.size(); ++i) {
-            nums[i] %= modulo;
-            pre += nums[i] == k;
+        int pre = 0;
+        for (int num: nums) {
+            if (num % modulo == k) {
+                pre++;
+                pre %= modulo;
+            }
+            int right = (pre - k + modulo) % modulo;
+            ans += mp[right];
+            mp[pre]++;
+        }
 
-            int query = ((pre % modulo - k) + modulo) % modulo;
-            ans = (ans + mp[query]);
-            mp[pre % modulo]++;
-        }   
-
-        return ans;     
+        return ans;
     }
 };
