2c997c7930b702ccf7d0a94022aecffa7bdf0b76
Yiming Pan <perfectpan325@gmail.com>
2025-05-06 13:24:49 +0800

    feature: add 1920 solution1 for C++

---
+++ solution/1920/sol2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            int x = nums[i];
+            if (x < 0) {
+                continue;
+            }
+            int cur = i;
+            while (nums[cur] != i) {
+                int nxt = nums[cur];
+                nums[cur] = ~nums[nxt];
+                cur = nxt;
+            }
+            nums[cur] = ~x;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            nums[i] = ~nums[i];
+        }
+        return nums;
+    }
+};
